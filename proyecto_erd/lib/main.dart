b5d15317a0db6cf681fd75dd3d0a73d96b863222
Daniel-Peralt4 <dperaltapenuela07@gmail.com>
2024-09-26 10:13:55 -0500

    Cambio de vista

--- proyecto_erd/lib/main.dart
+++ proyecto_erd/lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:proyecto_erd/pages/gestionPrecios_page.dart';
-import 'package:proyecto_erd/pages/gestionUsuarios_page.dart';
 import 'package:proyecto_erd/pages/login_page.dart';
 
 void main() {
@@ -16,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: GestionUsuariosPage(),
+      home: LoginPage(),
     );
   }
 }
