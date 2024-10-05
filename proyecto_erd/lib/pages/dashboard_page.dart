289fa4b153f7be28bd8e19ff1f3dbf2bca2f96a2
aeduardogonzalez <aeduardogonzalez@unicesar.edu.co>
2024-10-05 11:50:58 -0500

    Navbar listo

--- proyecto_erd/lib/pages/dashboard_page.dart
+++ proyecto_erd/lib/pages/dashboard_page.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:proyecto_erd/Controllers/check_controller.dart';
 import 'package:proyecto_erd/Controllers/calendar_controller.dart';
-import 'package:proyecto_erd/Controllers/navbar_controller.dart';
-import 'package:proyecto_erd/pages/gestionUsuarios_page.dart';
-import 'package:proyecto_erd/pages/gestionPrecios_page.dart';
-import 'package:proyecto_erd/pages/question_page.dart';
 
 class DashboardPage extends StatelessWidget {
   final String username;
@@ -17,7 +13,6 @@ class DashboardPage extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => CheckController()), // Provee el controlador del checklist
-        ChangeNotifierProvider(create: (context) => NavbarController()), // Provee el controlador del navbar
       ],
       child: Scaffold(
         appBar: AppBar(
@@ -34,89 +29,43 @@ class DashboardPage extends StatelessWidget {
           automaticallyImplyLeading: false, // Quitar la flecha de regresar
         ),
         backgroundColor: Color(0xFFDCE9FD),
-        body: Consumer<NavbarController>(
-          builder: (context, navbarController, child) {
-            final List<Widget> pages = [
-              _buildDashboardContent(context), // Página principal (dashboard)
-              GestionPreciosPage(), // Página adicional 1
-              GestionUsuariosPage(), // Página adicional 2
-              QuestionPage(), // Página adicional 3
-            ];
-            return pages[navbarController.selectedIndex];
-          },
-        ),
-        bottomNavigationBar: Consumer<NavbarController>(
-          builder: (context, navbarController, child) {
-            return BottomNavigationBar(
-              currentIndex: navbarController.selectedIndex,
-              onTap: navbarController.onTabTapped,
-              selectedItemColor: const Color.fromARGB(255, 0, 0, 0), // Color para el ítem seleccionado
-              unselectedItemColor: const Color.fromARGB(228, 158, 158, 158), // Color para ítems no seleccionados
-              
-              items: const [
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.business),
-                  label: 'Dashboard',
-                ),
-                BottomNavigationBarItem( 
-                  icon: Icon(Icons.business),
-                  label: 'Precios',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.business),
-                  label: 'Usuarios',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.business),
-                  label: 'Ayuda',
-                ),
-              ],
-            );
-          },
-        ),
-      ),
-    );
-  }
+        body: Column(
+          children: [
+            // Texto de bienvenida
+            Text(
+              'Bienvenido, $username!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 20),
+             // Aquí se integra CalendarView
+            SizedBox(
+              height: 150, // Ajusta este valor según sea necesario
+              child: CalendarView(),
+            ),
 
-  // Contenido de la página de Dashboard
-  Widget _buildDashboardContent(BuildContext context) {
-    return Column(
-      children: [
-        // Texto de bienvenida
-        Text(
-          'Bienvenido, $username!',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          textAlign: TextAlign.center,
-        ),
-        SizedBox(height: 20),
-
-        // Integración del CalendarView
-        SizedBox(
-          height: 150, // Ajusta este valor según sea necesario
-          child: CalendarView(),
-        ),
-
-        // Checklist
-        Expanded(
-          
-          child: Consumer<CheckController>(
-            builder: (context, checkController, child) {
-              return ListView.builder(
-                itemCount: checkController.tasks.length,
-                itemBuilder: (context, index) {
-                  return CheckboxListTile(
-                    title: Text(checkController.tasks[index]["title"]),
-                    value: checkController.tasks[index]["isChecked"],
-                    onChanged: (value) {
-                      checkController.toggleTask(index, value);
+            // Checklist
+            Expanded(
+              child: Consumer<CheckController>(
+                builder: (context, checkController, child) {
+                  return ListView.builder(
+                    itemCount: checkController.tasks.length,
+                    itemBuilder: (context, index) {
+                      return CheckboxListTile(
+                        title: Text(checkController.tasks[index]["title"]),
+                        value: checkController.tasks[index]["isChecked"],
+                        onChanged: (value) {
+                          checkController.toggleTask(index, value);
+                        },
+                      );
                     },
                   );
                 },
-              );
-            },
-          ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
---
+++ proyecto_erd/lib/pages/home_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_erd/pages/dashboard_page.dart';
+import 'package:proyecto_erd/pages/gestionPrecios_page.dart';
+import 'package:proyecto_erd/pages/gestionUsuarios_page.dart';
+import 'package:proyecto_erd/pages/question_page.dart';
+
+class HomePage extends StatefulWidget {
+
+  final String username; // Recibe el atributo desde LoginPage
+
+  HomePage({required this.username}); // Constructor para recibir el username
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  late List<Widget> _pages;
+
+  @override
+  void initState() {
+    super.initState();
+    // Pasamos el username a DashboardPage
+    _pages = [
+      DashboardPage(username: widget.username), // Se pasa el username
+      GestionPreciosPage(),
+      GestionUsuariosPage(),
+      QuestionPage(),
+    ];
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex], // Mostrar la página seleccionada
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Precios',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Usuarios',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Ayuda',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor:
+            Theme.of(context).primaryColor, // Color cuando está seleccionado
+        unselectedItemColor: Colors.grey, // Color cuando no está seleccionado
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+// Ejemplo de las páginas
+
+class Page3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Página 3'),
+    );
+  }
+}
